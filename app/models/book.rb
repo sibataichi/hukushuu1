858ff53c6b868b6cc75e2f9ae49e314c333dc6a9
EC2 Default User <ec2-user@ip-172-31-42-169.ec2.internal>
2022-06-02 07:20:55 +0000

    ouyou3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
   validates :title,presence:true
@@ -8,4 +10,9 @@ class Book < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
